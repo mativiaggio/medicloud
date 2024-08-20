9c40c5cd52c2f2fd5626d153ebc507a940b80eaf
Matias Viaggio <matiasviaggio59@gmail.com>
2024-08-20 01:11:16 -0300

    Home table and skeleton

--- app/(protected)/api/guest/route.ts
+++
@@ -1,8 +0,0 @@
-import api from "@/appwrite/appwrite";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const appwrite = await api.getAllGuestsDocuments([]);
-
-  return NextResponse.json(appwrite);
-}
