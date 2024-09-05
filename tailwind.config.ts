b864ae5d2472d7f3ded06d8e3b0180833e34d35a
Matias Viaggio <matiasviaggio59@gmail.com>
2024-09-05 01:44:43 -0300

    Medication screen and collection.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -156,11 +156,17 @@ const config = {
           "0%,70%,100%": { opacity: "1" },
           "20%,50%": { opacity: "0" },
         },
+        swing: {
+          "0%": { transform: "rotate(0deg)" },
+          "50%": { transform: "rotate(180deg)" },
+          "100%": { transform: "rotate(0deg)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "caret-blink": "caret-blink 1.25s ease-out infinite",
+        swing: "swing 3s ease-in-out infinite",
       },
       typography: {
         DEFAULT: {
